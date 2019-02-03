a8a48ba3277715d663d5d6bab32779a9b84a0293
Cameron <developer.cameron@gmail.com>
2019-02-02 18:21:45 -0700

    Remove new (#1)

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Todo app today
+            Todo app
           </p>
           <a
             className="App-link"
