04214c672019ad6247647c04c6bc79df0dc36e50
Andy Zalit <andyzalit@gmail.com>
2019-02-02 18:16:23 -0700

    block push to master test

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Todo app 
+            Todo app today
           </p>
           <a
             className="App-link"
